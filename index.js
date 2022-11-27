dea9bd02b6aaa45a204cd8e425c612d0c25ba1c5
+ <+>
2022-11-27 09:52:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, surprise){
+    for(let i=0; i<name.length; i++){
+        name[i]=`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`;
+    }
+    return name;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "Surprise");
+
+function countDown(number){
+    while(number>=0){
+        console.log (number--);
+        debugger;
+    }
+}
+countDown(4)
